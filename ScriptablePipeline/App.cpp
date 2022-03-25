0ccb807e0fad7eaa02f0e563745080ccef63d9ae
TheScientist14 <37724964+TheScientist14@users.noreply.github.com>
2022-03-25 12:15:07 +0100

    camera is working !!!!

--- ScriptablePipeline/App.cpp
+++ ScriptablePipeline/App.cpp
@@ -237,28 +237,42 @@ void App::handle_events() {
                             printf("%f, %f, %f", eulerAngles.x, eulerAngles.y, eulerAngles.z);
                         }
                         // 0 -90 0 ~ 180 -90- 180
-                        eulerAngles.x += -curEvent.motion.yrel * mouseSensitivity * deltaTime;
-                        eulerAngles.y += -curEvent.motion.xrel * mouseSensitivity * deltaTime;
                         if(abs(eulerAngles.z) < epsilon){
+                            eulerAngles.x += -curEvent.motion.yrel * mouseSensitivity * deltaTime;
+                            eulerAngles.y += -curEvent.motion.xrel * mouseSensitivity * deltaTime;
                             if(eulerAngles.y < -90){
                                 eulerAngles.x += 180;
-                                eulerAngles.y += 180;
-                                eulerAngles.z = 180;
+                                eulerAngles.y = -eulerAngles.y - 180;
+                                eulerAngles.z += 180;
                             }
                             else if(eulerAngles.y > 90){
                                 eulerAngles.x += 180;
-                                eulerAngles.y -= 180;
-                                eulerAngles.z = 180;
+                                eulerAngles.y = -eulerAngles.y + 180;
+                                eulerAngles.z += 180;
+                            }
+                        }
+                        else{
+                            eulerAngles.x += curEvent.motion.yrel * mouseSensitivity * deltaTime;
+                            eulerAngles.y += curEvent.motion.xrel * mouseSensitivity * deltaTime;
+                            if(eulerAngles.y < -90){
+                                eulerAngles.x += 180;
+                                eulerAngles.y = -eulerAngles.y + 180;
+                                eulerAngles.z += 180;
+                            }
+                            else if(eulerAngles.y > 90){
+                                eulerAngles.x += 180;
+                                eulerAngles.y = -eulerAngles.y - 180;
+                                eulerAngles.z += 180;
                             }
                         }
 
                         if(abs(eulerAngles.y) > 80){
                             printf(" %f, %f, %f\n", eulerAngles.x, eulerAngles.y, eulerAngles.z);
                         }
-
                         quat quatPitch = angleAxis(radians(eulerAngles.x), vec3(1,0,0));
                         quat quatYaw = angleAxis(radians(eulerAngles.y), vec3(0,1,0));
-                        mainCamera->transform->setRotation(mat4_cast(normalize(quatYaw * quatPitch)));
+                        quat quatRoll = angleAxis(radians(eulerAngles.z), vec3(0,0,1));
+                        mainCamera->transform->setRotation(mat4_cast(normalize(quatRoll * quatYaw * quatPitch)));
                     }
                 }
                 break;
