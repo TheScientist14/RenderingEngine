d65a78ea1f7f5c710742dd60958272f8db21788a
TheScientist14 <37724964+TheScientist14@users.noreply.github.com>
2022-03-30 17:28:39 +0200

    end of merge

--- ScriptablePipeline/App.h
+++ ScriptablePipeline/App.h
@@ -25,7 +25,7 @@ class Quad;
 class Cube;
 class GameObject;
 class Skybox;
-class WorldGeneration;
+class ChunkGeneration;
 
 typedef shared_ptr<Geometry> Sp_Geometry;
 typedef shared_ptr<Texture> Sp_Texture;
@@ -70,7 +70,7 @@ protected:
     vector<shared_ptr<RenderedObject>> objectsToRender;
     shared_ptr<Camera> mainCamera;
     bool isMouseCaptured = true;
-    WorldGeneration* world;
+    ChunkGeneration* world;
     vector<shared_ptr<Quad>> generatedQuads;
     vector<shared_ptr<Cube>> generatedCubes;
     bool isDragging;
